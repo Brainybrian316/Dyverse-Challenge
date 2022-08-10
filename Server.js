c376b7e07a82ab90537551a3e23fc1a5728c6330
Brainybrian316 <brainybrian316@gmail.com>
2022-08-10 19:08:09 -0400

    anticipated server set up

--- Server.js
+++ Server.js
@@ -1,1 +1,40 @@
 
+// Modules to require
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+const path = require('path');
+const session = require('express-session');
+
+
+//  initial variables declared for use in the server
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// middleware
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+//  set up session with cookies and sequelize
+
+const sess = {
+  secret: 'keyboard cat',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+})
+};
+
+app.use(session(sess));
+// use express json to parse json data
+app.use(express.json());
+//  use to parse urlencoded data
+app.use(express.urlencoded({ extended: true }));
+//  set up routes and controllers
+app.use(routes);
+
+// turn on connection to the database
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}`));
+})
