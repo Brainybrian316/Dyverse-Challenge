88e8190382aeb7744748d1923a6bc4d1baafdf90
Brainybrian316 <brainybrian316@gmail.com>
2022-08-10 23:31:01 -0400

    new data is inserted at the end of the tree

--- Controller/api.js
+++ Controller/api.js
@@ -4,10 +4,15 @@ const { Root, Animals, Children, StepChildren } = require('../Model');
 
 router.get('/tree', (req, res) => {
   Root.findAll({
+    // new id's are sent to the bottom of the tree
+    order: [['id', 'DESC']],
+    // we want to include the data for the animals and children
     include: [
       {
       model: Animals,
+      // the data we want to include from the animals table
       attributes: ['label'],
+      // we want to include the data for the children table
       include: [
         {
           model: Children,
@@ -23,24 +28,40 @@ router.get('/tree', (req, res) => {
     },
   ],
   })
+  // send the data to the client
     .then(dbRootData => res.json(dbRootData))
+    // if there is an error, catch the error
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
-// check to make sure animal data is displaying correctly
-router.get('/animals', (req, res) => {
-  Animals.findAll()
+// post a new animal/parent
+router.post('/tree', (req, res) => {
+  // create a new animal/parent with the data from the client
+  Animals.create(req.body) // req.body takes in label and rootId
     .then(dbAnimalData => res.json(dbAnimalData))
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     }
   );
-}
-);
+})
+
+// // check to make sure animal data is displaying correctly
+// router.get('/animals', (req, res) => {
+//   Animals.findAll()
+//     .then(dbAnimalData => res.json(dbAnimalData))
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     }
+//   );
+// }
+// );
+
+
 
 
 module.exports = router;
