79293f38b1e5d509a43f1b62df73a7842e635123
Brainybrian316 <brainybrian316@gmail.com>
2022-08-13 17:05:09 -0400

    reformatted test to match initial challenge with 9 passing test

--- MineSweeper/__tests__/helper.test.js
+++ MineSweeper/__tests__/helper.test.js
@@ -27,75 +27,57 @@ test('mineSweeper', () => {
   expect(mineSweeper(input)).toEqual(output);
 });
 
-// test('mineSweeper', () => {
-//   const input2 = [
-//    ' * * ', '     ', '   * ', '  * *',
-//            ' * * ',
-//   ]
-//   const output2 = [
-//      '1*2*1', '11322', ' 12*2', '12*4*',
-//            '1*3*2', 
-//   ];
-//   expect(mineSweeper(input2)).toEqual(output2);
-// });
+test('mineSweeper', () => {
+  const input2 = ['+-----+', '| * * |', '|     |', '|   * |', '|  * *|',
+  '| * * |', '+-----+']
+  const output2 = ['+-----+', '|1*2*1|', '|11322|', '| 12*2|', '|12*4*|',
+  '|1*3*2|', '+-----+']
+  expect(mineSweeper(input2)).toEqual(output2);
+});
 
-// test('mineSweeper', () => {
-//   const input3 = [
-//    ' * * ',
-//   ];
-//   const output3 = [
-//    '1*2*1',
-//   ];
-//   expect(mineSweeper(input3)).toEqual(output3);
-// });
+test('mineSweeper', () => {
+  const input3 =  ['+-----+', '| * * |', '+-----+']
+  const output3 =  ['+-----+', '|1*2*1|', '+-----+']
+  expect(mineSweeper(input3)).toEqual(output3);
+});
 
-// test('mineSweeper', () => {
-//   const input4 = [
-//    '*', ' ', '*', ' ', ' ',
-//   ];
-//   const output4 = [ '*', '2', '*', '1', ' '];
-//   expect(mineSweeper(input4)).toEqual(output4);
-// });
+test('mineSweeper', () => {
+  const input4 =  ['+-+', '|*|', '| |', '|*|', '| |', '| |', '+-+']
+  const output4 =  ['+-+', '|*|', '|2|', '|*|', '|1|', '| |', '+-+']
+  expect(mineSweeper(input4)).toEqual(output4);
+});
 
-// test('mineSweeper', () => {
-//   const input5 = [
-//      '*', 
-//   ];
-//   const output5 = [
-//      '*', 
-//   ];
-//   expect(mineSweeper(input5)).toEqual(output5);
-// });
+test('mineSweeper', () => {
+  const input5 = ['+-+', '|*|', '+-+']
+  const output5 = ['+-+', '|*|', '+-+']
+  expect(mineSweeper(input5)).toEqual(output5);
+});
 
-// test('mineSweeper', () => {
-//   const input6 = [
-//      '**', '**', 
-//   ]
-//   const output6 = [
-//      '**', '**',
-//   ];
-//   expect(mineSweeper(input6)).toEqual(output6);
-// });
+test('mineSweeper', () => {
+  const input6 = ['+--+', '|**|', '|**|', '+--+']
+  const output6 = ['+--+', '|**|', '|**|', '+--+']
+  expect(mineSweeper(input6)).toEqual(output6);
+});
 
-// test('mineSweeper', () => {
-//   const input7 = [ '**', '**',]
-//   const output7 = [ '**', '**',]
-//   expect(mineSweeper(input7)).toEqual(output7);
-// });
+test('mineSweeper', () => {
+  const input7 = ['+--+', '|**|', '|**|', '+--+']
+  const output7 = ['+--+', '|**|', '|**|', '+--+']
+  expect(mineSweeper(input7)).toEqual(output7);
+});
 
-// test('mineSweeper', () => {
-//   const input8 = [ '***', '* *', '***',]
-//   const output8 = [ '***', '*8*', '***',]
-//   expect(mineSweeper(input8)).toEqual(output8);
-// });
+test('mineSweeper', () => {
+  const input8 =  ['+---+', '|***|', '|* *|', '|***|', '+---+']
+  const output8 = ['+---+', '|***|', '|*8*|', '|***|', '+---+']
+  expect(mineSweeper(input8)).toEqual(output8);
+});
 
-// test('mineSweeper', () => {
-//   const input9 = ['     ', '   * ', '     ', '     ',
-//   ' *   ',]
-// const output9 = [ '  111', '  1*1', '  111', '111  ',
-//   '1*1  ']
-//   expect(mineSweeper(input9)).toEqual(output9);
-// });
+test('mineSweeper', () => {
+  const input9 =  ['+-----+', '|     |', '|   * |', '|     |', '|     |',
+  '| *   |', '+-----+']
+const output9 = ['+-----+', '|  111|', '|  1*1|', '|  111|', '|111  |',
+'|1*1  |', '+-----+']
+  expect(mineSweeper(input9)).toEqual(output9);
+});
 
 
 // test('boundary', () => {
